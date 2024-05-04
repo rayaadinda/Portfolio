57cf87471c360eda606574753298eb2915eed90e
rayaadinda <rayaadinda78@gmail.com>
2024-05-04 10:11:44 +0700

    adding footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
+import Footer from "@/components/footer";
 
 const roboto = Roboto({ 
   subsets: ["latin"],
