8cd8f1f8ce96a237c005e02d984e58022093c4fe
rayaadinda <rayaadinda78@gmail.com>
2024-05-29 23:02:41 +0700

    fix cv download button

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 		<html lang="en" className="!scroll-smooth">
 			<body
 				className={`${roboto.className}
-      bg-gray-50 text-gray-950 relative pt-40 sm:pt-48 bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+       text-gray-950 relative pt-40 sm:pt-48 bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
 			>
 				<div
 					className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] 
