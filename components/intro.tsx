27fb75d1985ca691a31cadebbb2dc424defa4cc2
rayaadinda <rayaadinda78@gmail.com>
2024-07-11 14:15:48 +0700

    fix file cv

--- components/intro.tsx
+++ components/intro.tsx
@@ -59,7 +59,7 @@ export default function Intro() {
 					className="group bg-white text-black px-7 py-3 flex items-center
         gap-2 rounded-full outline-none focus:scale-110 hover:scale-110
         active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-					href="RayaAdindaJayadiAhmad_MSIB_CV.PDF"
+					href="/CV.pdf"
 					download
 				>
 					Download CV{" "}
